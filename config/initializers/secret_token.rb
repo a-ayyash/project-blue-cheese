f7fb0465741f14694f0c479177d1278ae9d5a990
Ayyash <ahmed.ayyash@gmail.com>
2012-05-09 11:00:30 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mesh8tracks::Application.config.secret_token = '9774d32f8193a80f01b0192e7096dd0c1412b1acc3da2770cc9c66bceb79e3e2f96e34c1b6a5f47ab546d02da284f827e3829125675d01bdf10234487ea4faa8'
